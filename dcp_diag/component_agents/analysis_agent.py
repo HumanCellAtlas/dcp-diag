f533ae817d845739d0d9e82bb6a55f53650de3d4
Rex Wang <rexwangcc@gmail.com>
2018-12-04 11:01:02 -0500

    1st iteration of the analysis agents and finders.

--- dcp_diag/component_agents/analysis_agent.py
+++ dcp_diag/component_agents/analysis_agent.py
@@ -1,4 +1,12 @@
+from cromwell_tools import api as cwm_api
+from cromwell_tools import cromwell_auth as cwm_auth
 
-# Contact Cromwell Tools API
 
-# TODO: Rex
+class AnalysisAgent:
+    def __init__(self, deployment):
+        """
+
+        Args:
+            deployment:
+        """
+        self.deployment = deployment
--- dcp_diag/finders/analysis_finder.py
+++ dcp_diag/finders/analysis_finder.py
@@ -5,8 +5,9 @@ from .finder import Finder
 class AnalysisFinder:
 
     """
-    dcpdig @analysis workflow=<id>
-    dcpdig @analysis project=<id>
+    dcpdig @analysis workflow_uuid=<id>
+    dcpdig @analysis project_uuid=<id>
+    dcpdig @analysis bundle_uuid=<id>
     """
 
     name = 'analysis'
@@ -15,13 +16,25 @@ class AnalysisFinder:
         self.deployment = deployment
 
     def find(self, expression):
+        """
+
+        Args:
+            expression:
+        """
         field_name, field_value = expression.split('=')
 
-        if field_name == 'workflow':
-            print("SAM: workflow!")
+        if field_name == 'workflow_uuid':
+            print("Searching for the workflow(s) associated with the workflow_uuid for you!")
 
-        elif field_name == 'project':
-            pass
+        elif field_name == 'project_uuid':
+            print("Searching for the workflow(s) associated with the project_uuid for you!")
+
+        elif field_name == 'bundle_uuid':
+            print("Searching for the workflow(s) associated with the primary bundle_uuid for you!")
+
+        else:
+            print(f"Sorry I don't know how to find a {field_name}")
+            exit(1)
 
 
 Finder.register(AnalysisFinder)

217dfa895afc2b2d045dd01c19417714fbfc7e24
Rex Wang <rexwangcc@gmail.com>
2019-01-23 22:24:18 -0500

    Make sure the existing component finders use absolute import; implement analysis finder.

--- dcp_diag/finders/finder.py
+++ dcp_diag/finders/finder.py
@@ -7,8 +7,12 @@ class Finder:
         cls._finders[finder_class.name] = finder_class
 
     @classmethod
-    def factory(cls, finder_name, deployment):
+    def factory(cls, finder_name, deployment, service_account_key=''):
         for name, finder in cls._finders.items():
             if name == finder_name:
-                return finder(deployment=deployment)
+                # TODO: simplify this by switching to OAuth
+                if service_account_key:
+                    return finder(deployment=deployment, service_account_key=service_account_key)
+                else:
+                    return finder(deployment=deployment)
         raise RuntimeError(f"Unknown finder: {finder_name}")

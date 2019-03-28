e103f21765b0951fcb14c08d80a99c7cbf3ec079
Alegria Aclan <aaclan@ebi.ac.uk>
2019-03-28 12:10:27 +0000

    Remove auth headers for GET requests to ingest

--- dcp_diag/component_agents/__init__.py
+++ dcp_diag/component_agents/__init__.py
@@ -1,6 +1,4 @@
 from .data_store_agent import DataStoreAgent
 from .ingest_api_agent import IngestApiAgent
-from .ingest_auth_agent import IngestAuthAgent
-from .ingest_ui_agent import IngestUIAgent
 from .azul_agent import AzulAgent
 from .analysis_agent import AnalysisAgent
--- dcp_diag/component_agents/ingest_api_agent.py
+++ dcp_diag/component_agents/ingest_api_agent.py
@@ -1,4 +1,3 @@
-from .ingest_auth_agent import IngestAuthAgent
 from .hateoas_agent import HateoasAgent
 
 
@@ -6,8 +5,7 @@ class IngestApiAgent(HateoasAgent):
 
     def __init__(self, deployment):
         self.deployment = deployment
-        super().__init__(api_url_base=self._ingest_api_url(),
-                         auth_headers=IngestAuthAgent().make_auth_header())
+        super().__init__(api_url_base=self._ingest_api_url())
 
     def _ingest_api_url(self):
         if self.deployment == 'prod':
